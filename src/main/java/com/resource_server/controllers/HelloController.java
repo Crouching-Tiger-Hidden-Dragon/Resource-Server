6a5b7766b37e01be25fff6551ee4aed55d7c8530
HUANYU.ZHANG <huan94220@gmail.com>
2021-07-08 18:10:53 -0400

    first commit

---
+++ src/main/java/com/resource_server/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.resource_server.controllers;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
+}
+
